0216b0202f4eb5cf1bf07f887304a77cc4261abb
muxcmux <storm.bg@gmail.com>
2023-11-16 01:28:35 +0000

    remove rust.vim

--- lua/plugins/filetypes.lua
+++ lua/plugins/filetypes.lua
@@ -3,6 +3,5 @@ return {
   { 'tpope/vim-rails', ft = { 'ruby', 'erb', 'haml' } },
   { 'vim-crystal/vim-crystal', ft = { 'crystal' } },
   { 'cespare/vim-toml', ft = { 'toml' } },
-  { 'rust-lang/rust.vim', ft = { 'rust' } },
   { 'mustache/vim-mustache-handlebars', ft = { 'mustache' } },
 }
