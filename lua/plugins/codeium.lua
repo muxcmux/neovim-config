bf1f1a473e0975d0178fade3bc813fcaaefccfc8
muxcmux <storm.bg@gmail.com>
2024-03-13 18:47:21 +0000

    add codeium

---
+++ lua/plugins/codeium.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'Exafunction/codeium.vim',
+    event = "BufEnter",
+    config = function()
+      vim.g.codeium_disable_bindings = 1
+      vim.g.codeium_filetypes = {
+        TelescopePrompt = false,
+        TelescopeResults = false
+      }
+      vim.keymap.set('i', '<C-f>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+      vim.keymap.set('i', '<C-n>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
+      vim.keymap.set('i', '<C-p>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
+      vim.keymap.set('i', '<C-d>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+    end
+  },
+}
