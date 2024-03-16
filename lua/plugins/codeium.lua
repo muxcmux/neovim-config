7db74a8fa2f158d8fcf39b9b77c3574abec5a173
muxcmux <storm.bg@gmail.com>
2024-03-16 18:00:05 +0000

    ignore ai for minifiles buffer

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -6,7 +6,8 @@ return {
       vim.g.codeium_disable_bindings = 1
       vim.g.codeium_filetypes = {
         TelescopePrompt = false,
-        TelescopeResults = false
+        TelescopeResults = false,
+        minifiles = false,
       }
       vim.keymap.set('i', '<C-f>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
       vim.keymap.set('i', '<C-n>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
